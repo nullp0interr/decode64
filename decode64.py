bb0ca25db1178de8697846fcf1860b7af3900b7a
p0interr <114043239+nullp0interr@users.noreply.github.com>
2026-04-27 22:52:01 +0300

    added decode64.py

---
+++ decode64.py
@@ -0,0 +1,23 @@
+from Cryptodome.Cipher import AES
+import base64
+
+print("--- AES Дешифратор ---")
+
+# get key and data
+key_input = input("Enter a key (Base64): ")
+data_input = input("Enter a encode data (Base64): ")
+
+try:
+    key = base64.b64decode(key_input)
+    data = base64.b64decode(data_input)
+
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    decrypted = cipher.decrypt(data).decode('utf-8').strip()
+
+    print(f"\n✅ encoded text: {decrypted}")
+
+except ValueError as e:
+    print(f"\n❌ error: uncorrect key or data not match with correct format, details: {e}")
+except Exception as e:
+    print(f"\n❌ unhandle exception: {e}")
